c91aa3a98c1ce7ae5c85120c7e388df5f4815c5f
孙正君 <spacex@bupt.edu.cn>
2022-05-21 17:02:00 +0800

    report.c

--- report.c
+++ report.c
@@ -1,8 +1,61 @@
-#include "definition.h"
-
+#include "definition.h"                                                         //2022.5.21 
+                                                                                //SunZhengjun
 extern State state;
 
 void report()
 {
-    
+       
+printf("BUS:\nposition:%d\ntarget: ");
+    State * p = State->target ;
+    if (!p)
+	{
+		return;
+	}
+	State * a = p;
+	do
+	{
+		if (a == p->next)
+		{
+			printf("%d", p->data);
+			return;
+		}
+		printf("%d", p->data);
+		p = p->next;
+	} while (1);
+
+printf("STATION:\nclockwise:");
+State * p = State->clockwise_request ;
+    if (!p)
+	{
+		return;
+	}
+	State * a = p;
+	do
+	{
+		if (a == p->next)
+		{
+			printf("%d", p->data);
+			return;
+		}
+		printf("%d", p->data);
+		p = p->next;
+	} while (1);
+printf("counterclockwise: ");
+  State * p = State->counterclockwise_request ;
+    if (!p)
+	{
+		return;
+	}
+	State * a = p;
+	do
+	{
+		if (a == p->next)
+		{
+			printf("%d", p->data);
+			return;
+		}
+		printf("%d", p->data);
+		p = p->next;
+	} while (1);
+
 }

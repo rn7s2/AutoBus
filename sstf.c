b8e620fcf110fdc9aa04e26d71acac92a5930845
rn7s2 <mail@rn7s2.cn>
2022-06-02 22:33:27 +0800

    Style SSTF.

--- sstf.c
+++ sstf.c
@@ -1,8 +1,11 @@
 #include "definition.h"
 #include "dispatcher.h"
+
 extern State state;
 extern Config config;
-int flag=0,flag1=0;
+
+int flag = 0, flag1 = 0;
+
 int fflag() // 找出最短路径
 {
     int a = 1;
@@ -10,37 +13,39 @@ int fflag() // 找出最短路径
     int c = 1, d = -1;
     int tipc = 0;
     int tipcc = 0;
+
     while(1) {
         if(state.position / config.distance + a >= config.total_station)
             a = -(state.position / config.distance);
-            if(a == 0)
+        if(a == 0)
             break;
-        if(state.clockwise_request[state.position / config.distance + a+1] == 1
-                || state.counterclockwise_request[state.position / config.distance + a+1] == 1
-                || state.target[state.position / config.distance + a+1] == 1) {
+        if(state.clockwise_request[state.position / config.distance + a + 1] == 1
+                || state.counterclockwise_request[state.position / config.distance + a + 1] == 1
+                || state.target[state.position / config.distance + a + 1] == 1) {
             tipc = 1;
             break;
         } else if(a != 0) {
             a++;
             b++;
-        } 
+        }
     }
 
     while(1) {
         if(state.position / config.distance + d <= -1)
             d = -(state.position / config.distance - config.total_station + 1);
-            if(d == 0)
+        if(d == 0)
             break;
-        if(state.clockwise_request[state.position / config.distance + d+1] == 1
-                || state.counterclockwise_request[state.position / config.distance + d+1] == 1
-                || state.target[state.position / config.distance + d+1] == 1) {
+        if(state.clockwise_request[state.position / config.distance + d + 1] == 1
+                || state.counterclockwise_request[state.position / config.distance + d + 1] == 1
+                || state.target[state.position / config.distance + d + 1] == 1) {
             tipcc = 1;
             break;
         } else if(d != 0) {
             d--;
             c++;
-        } 
+        }
     }
+
     if(tipc == 1 && tipcc == 1 && c < b)
         return (-c);
     if(tipc == 1 && tipcc == 1 && c >= b)
@@ -54,27 +59,24 @@ void sstf_clock_tick()
     state.time++;
     if(state.state == 0 && state.current_target != 0) {
         if(state.current_target > 0) {
-
             state.state = 3;
         } else if(state.current_target < 0) {
-
             state.state = 1 ;
         }
     }
     state.last_state = state.state ;
     if(state.state == 3) {
-        if(state.position != (config.distance * config.total_station) - 1)
+        if(state.position != (config.distance * config.total_station) - 1) {
             state.position++;
-        else
+        } else {
             state.position = 0;
-    }
-     else if(state.state == 1) {
+        }
+    } else if(state.state == 1) {
         if(state.position == 0)
             state.position = config.distance * config.total_station - 1;
         else
             state.position--;
-    }
-    else if (state.state == 2) {
+    } else if (state.state == 2) {
         if(state.current_target == 0)
             state.current_target = fflag();
         if (state.current_target > 0)
@@ -83,8 +85,6 @@ void sstf_clock_tick()
             state.state = 1;
         else if(state.current_target == 0)
             state.state = 0;
-
-
     }
     if (state.position % config.distance == 0 && state.last_state != 2
             && state.last_state != 0) {
@@ -96,51 +96,45 @@ void sstf_clock_tick()
             state.current_target--;
         }
         if ((state.state == 1
-                &&             (state.target[state.position / config.distance+1] == 1 ||
-                                state.counterclockwise_request[state.position / config.distance+1] == 1))||state.current_target==0) {
+                &&             (state.target[state.position / config.distance + 1] == 1 ||
+                                state.counterclockwise_request[state.position / config.distance + 1] == 1)) || state.current_target == 0) {
             state.state = 2;
-            flag=1;
+            flag = 1;
 
-            if (state.current_target==0)
-            {
-                flag1=1;
-                
+            if (state.current_target == 0) {
+                flag1 = 1;
+
             }
-        }
-        else if ((state.state == 3
-                &&             (state.target[state.position / config.distance+1] == 1 ||
-                                state.clockwise_request[state.position / config.distance+1] == 1))||state.current_target==0) {
+        } else if ((state.state == 3
+                    &&             (state.target[state.position / config.distance + 1] == 1 ||
+                                    state.clockwise_request[state.position / config.distance + 1] == 1)) || state.current_target == 0) {
             state.state = 2;
-            flag=3;
-            
-            if (state.current_target==0)
-            {
-                flag1=1;
-                
+            flag = 3;
+
+            if (state.current_target == 0) {
+                flag1 = 1;
+
             }
         }
 
     }
-if(state.position % config.distance == 0 && state.last_state == 2){
-    if (flag1==1){
-    
-     state.counterclockwise_request[state.position / config.distance+1] = 0;
-     state.target[state.position / config.distance+1] = 0;
-            state.clockwise_request[state.position / config.distance+1] = 0;}
-            else 
-            {
-                if (flag == 1)
-                {
-                     state.target[state.position / config.distance+1] = 0;
-            state.counterclockwise_request[state.position / config.distance+1] = 0;
-                }
-                else if(flag==3){state.target[state.position / config.distance+1] = 0;
-            state.clockwise_request[state.position / config.distance+1] = 0;
-                }
+    if(state.position % config.distance == 0 && state.last_state == 2) {
+        if (flag1 == 1) {
+            state.counterclockwise_request[state.position / config.distance + 1] = 0;
+            state.target[state.position / config.distance + 1] = 0;
+            state.clockwise_request[state.position / config.distance + 1] = 0;
+        } else {
+            if (flag == 1) {
+                state.target[state.position / config.distance + 1] = 0;
+                state.counterclockwise_request[state.position / config.distance + 1] = 0;
+            } else if(flag == 3) {
+                state.target[state.position / config.distance + 1] = 0;
+                state.clockwise_request[state.position / config.distance + 1] = 0;
             }
-            flag1=0;
-            flag=0;
-}
+        }
+        flag1 = 0;
+        flag = 0;
+    }
 }
 
 void sstf_primary_request(int direction, int station)
